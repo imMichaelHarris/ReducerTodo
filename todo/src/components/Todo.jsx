b50f5dbb21d663836b136081a05a0d9a348a01ff
Michael Harris <mhguitar1997@gmail.com>
2019-07-23 13:55:10 -0700

    Todo component adds completed classname when completed is true

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const Todo = ({todo, dispatch}) => {
-    const toggle = () => dispatch({type: "TOGGLE_TODO", payload: todo.id})
-    return (
-        <div onClick={toggle} >
-            <h2>{todo.item}</h2>
-        </div>
-    );
+const Todo = ({ todo, dispatch }) => {
+  const toggle = () => dispatch({ type: "TOGGLE_TODO", payload: todo.id });
+  return (
+    <div onClick={toggle} className={todo.completed && "completed"}>
+      <h2>{todo.item}</h2>
+    </div>
+  );
 };
 
 export default Todo;
