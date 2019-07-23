198ce37da5a26a6b01fdd7388d41f9e765ff2cad
Michael Harris <mhguitar1997@gmail.com>
2019-07-23 12:28:14 -0700

    Import TodoList component in App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import TodoList from './views/TodoList';
 
 function App() {
   return (
     <div className="App">
-
+      <TodoList />
 
     </div>
   );
