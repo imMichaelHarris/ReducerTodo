938a4a72ac3db663177213244242742a04c3725c
Michael Harris <mhguitar1997@gmail.com>
2019-07-23 13:38:08 -0700

    DOESN'T WORK: attempt to toggle todo case

--- todo/src/reducers/todoReducer.js
+++ todo/src/reducers/todoReducer.js
@@ -8,8 +8,17 @@ export const initialState = [
 
 export const todoReducer = (state, action) => {
   switch (action.type) {
-      case "ADD_TODO":
-          return [...state, action.payload]
+    case "ADD_TODO":
+      return [...state, action.payload];
+    case "TOGGLE_TODO":
+      return [
+        ...state,
+        state.map(todo => {
+          return (
+            todo.id === action.payload && { ...todo, completed: !todo.completed }
+          );
+        })
+      ];
     default:
       return state;
   }
