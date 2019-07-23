0e3e62161351cf8118fe6be34e445e01d78336d7
Michael Harris <mhguitar1997@gmail.com>
2019-07-23 12:45:55 -0700

    Form input now controlled by useState hook

--- todo/src/components/TodoForm.jsx
+++ todo/src/components/TodoForm.jsx
@@ -1,13 +1,18 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const TodoForm = ({dispatch}) => {
-    return (
-        <form>
-            <legend>Add Todo Item</legend>
-            <input autoComplete="off" />
-            <button>Add</button>
-        </form>
-    );
+const TodoForm = ({ dispatch }) => {
+  const [item, setItem] = useState();
+
+  const handleChange = e => {
+    setItem(e.target.value);
+  };
+  return (
+    <form>
+      <legend>Add Todo Item</legend>
+      <input autoComplete="off" onChange={handleChange} value={item} />
+      <button>Add</button>
+    </form>
+  );
 };
 
 export default TodoForm;
