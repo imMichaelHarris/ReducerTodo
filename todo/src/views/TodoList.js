1ea2c2f6d2e915de8ae3d16a7c2d3f669a2facc2
Michael Harris <mhguitar1997@gmail.com>
2019-07-23 14:37:18 -0700

    Function clearCompleted create that will dispatch

--- todo/src/views/TodoList.js
+++ todo/src/views/TodoList.js
@@ -1,18 +1,21 @@
 import React, { useReducer } from "react";
 import { initialState, todoReducer } from "../reducers/todoReducer";
-import Todo from '../components/Todo'
+import Todo from "../components/Todo";
 import TodoForm from "../components/TodoForm";
 
 const TodoList = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState);
+
+  const clearCompleted = () => dispatch({ type: "CLEAR_COMPLETED" });
+  
   return (
     <div>
-        <header>
+      <header>
         <TodoForm dispatch={dispatch} />
-        <button>Clear Completed</button>
-        </header>
+        <button onClick={clearCompleted}>Clear Completed</button>
+      </header>
       {todos.map(todo => (
-        <Todo key={todo.id} todo={todo} dispatch={dispatch}/>
+        <Todo key={todo.id} todo={todo} dispatch={dispatch} />
       ))}
     </div>
   );
