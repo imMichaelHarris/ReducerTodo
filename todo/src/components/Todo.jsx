63577214aab92d520f27c1932df5816ab2fb4f53
Michael Harris <mhguitar1997@gmail.com>
2019-07-23 16:05:36 -0700

    If date has passed due date is red

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import moment from 'moment'
+import moment from "moment";
 
 const Todo = ({ todo, dispatch }) => {
   const toggle = () => dispatch({ type: "TOGGLE_TODO", payload: todo.id });
+  const formattedTime = moment().format();
+//   console.log('format time', typeof formattedTime)
+//   console.log('dueat', typeof todo.dueAt)
   return (
     // <div onClick={toggle} className={todo.completed && "completed"}>
     <div onClick={toggle} className={todo.completed ? "completed" : null}>
       <h2>{todo.item}</h2>
-      <p>{moment().to(todo.dueAt)}</p>
+      <p className={todo.dueAt <= formattedTime ? "due" : ""}>
+        {moment().to(todo.dueAt)}
+      </p>
     </div>
   );
 };
+
 
 export default Todo;
--- todo/src/components/TodoForm.jsx
+++ todo/src/components/TodoForm.jsx
@@ -1,36 +1,51 @@
 import React, { useState } from "react";
-import DatePicker from 'react-datepicker';
-import moment from  'moment'
-
+import DatePicker from "react-datepicker";
+import moment from "moment";
 
 const TodoForm = ({ dispatch }) => {
-  const [item, setItem] = useState("");
+  const [item, setItem] = useState({
+    item: "",
+    dueDate: ""
+  });
 
   const handleChange = e => {
-    setItem(e.target.value);
+    setItem({
+      ...item,
+      [e.target.name]: e.target.value
+    });
   };
 
   const addItem = e => {
     e.preventDefault();
     const newItem = {
-      item: item,
+      item: item.item,
       completed: false,
       id: new Date(),
-      dueAt: moment().add(2, 'days').toDate()
-
+      // dueAt: new Date()
+      dueAt: moment().format(item.dueDate)
     };
     dispatch({ type: "ADD_TODO", payload: newItem });
-    console.log(newItem)
-    setItem("");
+    // console.log(newItem);
+    setItem({
+      item: "",
+      dueDate: ""
+    });
   };
   return (
     <form onSubmit={addItem}>
       <legend>Add Todo Item</legend>
       <input
         type="text"
+        name="item"
         autoComplete="off"
         onChange={handleChange}
-        value={item}
+        value={item.item}
+      />
+      <input
+        type="date"
+        name="dueDate"
+        onChange={handleChange}
+        value={item.dueDate}
       />
       <button>Add</button>
     </form>
