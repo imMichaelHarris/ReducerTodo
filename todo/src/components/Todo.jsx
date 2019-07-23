95210ac3640c739d0048f9cf0442a0ec151c78cb
Michael Harris <mhguitar1997@gmail.com>
2019-07-23 13:22:36 -0700

    On click add to todo component that dispatches

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Todo = ({todo, dispatch}) => {
     const toggle = () => dispatch({type: "TOGGLE_TODO", payload: todo.id})
     return (
-        <div>
+        <div onClick={toggle} >
             <h2>{todo.item}</h2>
         </div>
     );
