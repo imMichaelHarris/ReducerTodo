6a406758733831d4a9d86c791022aa70dceb8d74
Michael Harris <mhguitar1997@gmail.com>
2019-07-23 12:32:58 -0700

    Todo component renders item name

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Todo = () => {
+const Todo = ({todo}) => {
     return (
         <div>
-            
+            <h2>{todo.item}</h2>
         </div>
     );
 };
